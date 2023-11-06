e49118feb623c79741de6bdfe4a6dc691cea95b7
jess1104 <jess1104jess1104@gmail.com>
2023-11-06 15:16:48 +0800

    feat:裝icon&國際化中文

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
   <div>
-    <h1>我是app根組件</h1>
+    <el-button type="primary" size="default" :icon="Search"> aaa </el-button>
+    <el-pagination
+      v-model:current-page="currentPage4"
+      v-model:page-size="pageSize4"
+      :page-sizes="[100, 200, 300, 400]"
+      :small="small"
+      :disabled="disabled"
+      :background="background"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Search } from "@element-plus/icons-vue";
+</script>
 
 <style lang="scss" scoped>
 div {
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,15 @@ import App from "./App.vue";
 // 引入element-plus
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+// 國際化語言
+// @ts-expect-error忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+import zhTw from "element-plus/dist/locale/zh-tw.mjs";
+
 // 獲取實例應用物件
 const app = createApp(App);
+
 // 使用element-plus插件
-app.use(ElementPlus);
+app.use(ElementPlus, {
+  locale: zhTw,
+});
 app.mount("#app");
