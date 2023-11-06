56f752948f465acb942d171041f05ba9464b77a7
jess1104 <jess1104jess1104@gmail.com>
2023-11-06 15:38:42 +0800

    feat:設置@為src的根目錄

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import App from "./App.vue";
+import App from "@/App.vue";
 // 引入element-plus
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
