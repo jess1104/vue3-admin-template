2612cdaf03887fe7c05018dcd48f10d43a26766d
jess1104 <jess1104jess1104@gmail.com>
2023-11-06 14:23:51 +0800

    feat:安裝element-plus初始

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-
-createApp(App).mount("#app");
+// 引入element-plus
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+// 獲取實例應用物件
+const app = createApp(App);
+// 使用element-plus插件
+app.use(ElementPlus);
+app.mount("#app");
