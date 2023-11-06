da6749b43213282b957b2f5364ed04aab170e65e
jess1104 <jess1104jess1104@gmail.com>
2023-11-06 15:39:13 +0800

    fix:刪掉不必要的值

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,9 @@
   <div>
     <el-button type="primary" size="default" :icon="Search"> aaa </el-button>
     <el-pagination
-      v-model:current-page="currentPage4"
-      v-model:page-size="pageSize4"
       :page-sizes="[100, 200, 300, 400]"
-      :small="small"
-      :disabled="disabled"
-      :background="background"
       layout="total, sizes, prev, pager, next, jumper"
       :total="400"
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
     />
   </div>
 </template>
